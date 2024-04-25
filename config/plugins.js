95f3ad453a8f14f95b4c066f20efcd9b990af2c3
Sandeep Gautam <sandeepgautam568@gmail.com>
2024-04-25 07:19:13 +0545

    fixed swagger docs and verified

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,33 @@
-module.exports = () => ({});
+module.exports = {
+    documentation: {
+        enabled: true,
+        config: {
+          openapi: '3.0.0',
+          info: {
+            version: '1.0.0',
+            title: 'PIRUS DOCUMENTATION',
+            description: '',
+            contact: {
+              name: 'Sandeep Gautam',
+              email: 'sandeep@infinitydevelopmententerprise.com',
+              url: 'infinitydevelopmententerprise.com'
+            },
+            license: {
+              name: 'Apache 2.0',
+              url: 'https://www.apache.org/licenses/LICENSE-2.0.html'
+            },
+          },
+          'x-strapi-config': {
+            // Leave empty to ignore plugins during generation
+            plugins: [ 'upload', 'users-permissions'],
+            path: '/documentation',
+          },
+          servers: [{ url: 'http://admin.pirus.app/api', description: 'Development server' }],
+          externalDocs: {
+            description: 'Find out more',
+            url: 'https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html'
+          },
+          security: [ { bearerAuth: [] } ]
+        }
+      }
+};
