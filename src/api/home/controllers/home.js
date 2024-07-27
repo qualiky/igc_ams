c23a0fbe97e26f9f44683c3a448b41760d098e3f
Sandeep Gautam <sandeepgautam568@gmail.com>
2024-07-27 22:39:01 +0545

    fixed home page api

--- src/api/home/controllers/home.js
+++ src/api/home/controllers/home.js
@@ -13,14 +13,16 @@ module.exports = createCoreController("api::home.home", ({ strapi }) => ({
       const receipts = await strapi.service("api::receipt.receipt").find();
       const clients = await strapi.service("api::client.client").find();
       const tickets = await strapi.service("api::ticket.ticket").find();
+      const employee = await strapi.service("api::employee.employee").find();
 
-      console.log(tickets);
+      console.log(employee);
 
       let data = {
         projects: projects?.results?.length ?? 0,
         receipts: receipts?.results?.length ?? 0,
         clients: clients?.results?.length ?? 0,
         tickets: tickets.results?.length ?? 0,
+        employee: employee?.results?.length ?? 0,
       };
 
       ctx.status = 200;
